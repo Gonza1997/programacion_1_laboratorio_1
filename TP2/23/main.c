8770b2ae043df0dc220eed47a8e065fc70210851
Gonza1997 <diazgonzalo2@live.com.ar>
2017-03-29 06:52:21 -0300

    Add files via upload

--- TP2/23/main.c
+++ TP2/23/main.c
@@ -3,13 +3,18 @@
 #include <conio.h>
 
 int numLegajo ();
-int validacion (int dato, int inferior, int superior);
+int validarEnt (int dato, int inferior, int superior);
+int sueldoNeto();
+char sexo();
 
 int main()
 {
     int legajo;
+    int sueldo;
+    int sexoA;
     legajo = numLegajo();
-
+    sueldo = sueldoNeto();
+    sexoA = sexo();
 
 
     return 0;
@@ -20,19 +25,44 @@ int numLegajo ()
     int dato;
     printf("Ingrese su legajo: ");
     scanf("%d", &dato);
-    dato = validacion(dato, 1000, 9999);
+    dato = validarEnt(dato, 1000, 9999);
     printf("Su legajo es: %d", dato);
 
     return dato;
 }
 
-int validacion (int dato, int inferior, int superior)
+int validarEnt (int dato, int inferior, int superior)
 {
     while(dato < inferior || dato > superior)
     {
-        printf("Por favor ingrese un dato entre %d y %d", inferior, superior);
+        printf("Por favor ingrese un dato entre %d y %d: ", inferior, superior);
         scanf("%d", &dato);
 
     }
     return dato;
+}
+
+int sueldoNeto()
+{
+    int dato;
+    printf("Por favor ingrese el sueldo: ");
+    scanf("%d", &dato);
+    dato = validarEnt(dato, 1500, 10000);
+    printf("Su sueldo es: %d", dato);
+    return dato;
+}
+
+char sexo()
+{
+    char sexo;
+    printf("Ingrese su sexo: ");
+    sexo = getch();
+
+    while(sexo != "f" && sexo != "m")
+    {
+        printf("Por favor ingrese 'f' o 'm': ");
+        sexo = getch();
+    }
+    printf("Su sexo es: %c", sexo);
+    return sexo;
 }
