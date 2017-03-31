55b5fa453434b1142aad9a403cd90e2252932be7
Gonza1997 <diazgonzalo2@live.com.ar>
2017-03-31 12:22:02 -0300

    Add files via upload

--- TP2/23/main.c
+++ TP2/23/main.c
@@ -7,7 +7,8 @@ int validarEnt (int dato, int inferior, int superior);
 int sueldoNeto();
 int edad();
 char sexo();
-int ejercicio1(char sexo, int sueldo);
+int ejercicio1(char sexo, int sueldo, int contador, int contadorM);
+int ejercicio2(char sexo, int edad, int contador);
 
 int main()
 {
@@ -18,7 +19,6 @@ int main()
     int contadorH = 0;
     int contadorM = 0;
     int contadorM38 = 0;
-
     char respuesta = 's';
 
     while(respuesta == 's')
@@ -27,7 +27,8 @@ int main()
         sueldo = sueldoNeto();
         sexoA = sexo();
         edadA = edad();
-        contadorH = ejercicio1(sexoA, sueldo);
+        contadorH = ejercicio1(sexoA, sueldo, contadorH, contadorM);
+        contadorM38 = ejercicio2(sexoA, edadA, contadorM38);
 
         printf("Desea seguir? (s/n) : ");
         respuesta = tolower(getch());
@@ -41,7 +42,7 @@ int main()
 int numLegajo ()
 {
     int dato;
-    printf("Ingrese su legajo: ");
+    printf("\nIngrese su legajo: ");
     scanf("%d", &dato);
     dato = validarEnt(dato, 1000, 9999);
     printf("Su legajo es: %d", dato);
@@ -53,7 +54,7 @@ int validarEnt (int dato, int inferior, int superior)
 {
     while(dato < inferior || dato > superior)
     {
-        printf("Por favor ingrese un dato entre %d y %d: ", inferior, superior);
+        printf("\nPor favor ingrese un dato entre %d y %d: ", inferior, superior);
         scanf("%d", &dato);
 
     }
@@ -63,39 +64,61 @@ int validarEnt (int dato, int inferior, int superior)
 int sueldoNeto()
 {
     int dato;
-    printf("Por favor ingrese el sueldo: ");
+    printf("\nPor favor ingrese el sueldo: ");
     scanf("%d", &dato);
     dato = validarEnt(dato, 1500, 10000);
-    printf("Su sueldo es: %d", dato);
+    printf("\nSu sueldo es: %d", dato);
     return dato;
 }
 
 char sexo()
 {
     char sexo;
-    printf("Ingrese su sexo: ");
+    printf("\nIngrese su sexo: ");
     sexo = tolower(getch());
 
     while(sexo != 'f' && sexo != 'm')
     {
-        printf("Por favor ingrese 'f' o 'm': ");
+        printf("\nPor favor ingrese 'f' o 'm': ");
         sexo = tolower(getch());
     }
-    printf("Su sexo es: %c", sexo);
+    printf("\nSu sexo es: %c", sexo);
     return sexo;
 }
 
 int edad()
 {
     int edad;
-    printf("Ingrese su edad: ");
+    printf("\nIngrese su edad: ");
     scanf("%d", &edad);
     edad = validarEnt(edad, 18, 65);
-    printf("Su edad es: %d", edad);
+    printf("\nSu edad es: %d", edad);
     return edad;
 }
 
-int ejercicio1(char sexo, int sueldo)
+int ejercicio1(char sexo, int sueldo, int contador, int contadorM)
 {
     if(sexo == 'm')
+    {
+        if(sueldo < 7500 && sueldo > 2000)
+        {
+            contador++;
+        }
+    }
+    else
+    {
+        contadorM++;
+    }
+    printf("\n%d", contador);
+    return contador;
+
+}
+
+int ejercicio2(char sexo, int edad, int contador)
+{
+    if(sexo == 'f' && edad < 38)
+    {
+        contador++;
+    }
+    return contador;
 }
